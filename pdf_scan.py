c7af232943dfe78a06166519e101fea166713972
Adrian <hadrian3689@gmail.com>
2022-01-11 23:22:26 -0500

    Added clases

--- pdf_scan.py
+++ pdf_scan.py
@@ -1,69 +1,78 @@
 import requests
 import os
 
-def get_users():
-    os.system("strings *.pdf | grep -i Creator | awk '{print $2}' | grep -v -i Tex | sed 's/(//' | sed 's/)//' > users.txt")
-    print("Users saved in file users.txt")
+class Intelligence():
+    def __init__(self,url):
+        self.url = url
+        self.get_pdfs()
+        self.get_users()
 
-def get_pedfs(url):
-    for month_first_digit in range(0,2):
-        if month_first_digit == 0:
-            for month_second_digit in range(1,10):
-                for day_first_digit in range(0,4):
-                    if day_first_digit == 3:
-                        for day_second_digit in range(0,2):
-                            final_url = url + str(month_first_digit) + str(month_second_digit) + "-" + str(day_first_digit) + str(day_second_digit) + "-upload.pdf"
-                            req_pdf = requests.get(final_url)
-                            if req_pdf.status_code != 404:
-                                filename = final_url.rsplit('/',1)[1] 
-                                download = "curl " + final_url + " -o " + filename + " >/dev/null 2>&1"
-                                print("Found file in URL: " + final_url + "\nDownloading: " + filename + "\n")
-                                os.system(download)
-                            else:
-                                continue
-                    else:
-                        for day_second_digit in range(0,10):
-                            final_url = url + str(month_first_digit) + str(month_second_digit) + "-" + str(day_first_digit) + str(day_second_digit) + "-upload.pdf"
-                            req_pdf = requests.get(final_url)
-                            if req_pdf.status_code != 404:
-                                filename = final_url.rsplit('/',1)[1] 
-                                download = "curl " + final_url + " -o " + filename + " >/dev/null 2>&1"
-                                print("Found file in URL: " + final_url + "\nDownloading: " + filename + "\n")
-                                os.system(download)
-                            else:
-                                continue
+    def get_users(self):
+        os.system("strings *.pdf | grep -i Creator | awk '{print $2}' | grep -v -i Tex | sed 's/(//' | sed 's/)//' > users.txt")
+        print("Users saved in file users.txt")
 
-        elif month_first_digit == 1:
-            for month_second_digit in range(0,3):
-                for day_first_digit in range(0,4):
-                    if day_first_digit == 3:
-                        for day_second_digit in range(0,2):
-                            final_url = url + str(month_first_digit) + str(month_second_digit) + "-" + str(day_first_digit) + str(day_second_digit) + "-upload.pdf"
-                            req_pdf = requests.get(final_url)
-                            if req_pdf.status_code != 404:
-                                filename = final_url.rsplit('/',1)[1] 
-                                download = "curl " + final_url + " -o " + filename + " >/dev/null 2>&1"
-                                print("Found file in URL: " + final_url + "\nDownloading: " + filename + "\n")
-                                os.system(download)
-                            else:
-                                continue
-                    else:
-                        for day_second_digit in range(0,10):
-                            final_url = url + str(month_first_digit) + str(month_second_digit) + "-" + str(day_first_digit) + str(day_second_digit) + "-upload.pdf"
-                            req_pdf = requests.get(final_url)
-                            if req_pdf.status_code != 404:
-                                filename = final_url.rsplit('/',1)[1] 
-                                download = "curl " + final_url + " -o " + filename + " >/dev/null 2>&1"
-                                print("Found file in URL: " + final_url + "\nDownloading: " + filename + "\n")
-                                os.system(download)
-                            else:
-                                continue
+    def get_pdfs(self):
+        for month_first_digit in range(0,2):
+            if month_first_digit == 0:
+                for month_second_digit in range(1,10):
+                    for day_first_digit in range(0,4):
+                        if day_first_digit == 3:
+                            for day_second_digit in range(0,2):
+                                final_url = self.url + str(month_first_digit) + str(month_second_digit) + "-" \
+                                    + str(day_first_digit) + str(day_second_digit) + "-upload.pdf"
+                                
+                                req_pdf = requests.get(final_url)
+                                if req_pdf.status_code != 404:
+                                    filename = final_url.rsplit('/',1)[1]
+                                    download = "curl " + final_url + " -o " + filename + " >/dev/null 2>&1"
+                                    print("Found file in URL: " + final_url + "\nDownloading: " + filename + "\n")
+                                    os.system(download)
+                                else:
+                                    continue
+                        else:
+                            for day_second_digit in range(0,10):
+                                final_url = self.url + str(month_first_digit) + str(month_second_digit) + "-" \
+                                    + str(day_first_digit) + str(day_second_digit) + "-upload.pdf"
 
-    get_users()
+                                req_pdf = requests.get(final_url)
+                                if req_pdf.status_code != 404:
+                                    filename = final_url.rsplit('/',1)[1]
+                                    download = "curl " + final_url + " -o " + filename + " >/dev/null 2>&1"
+                                    print("Found file in URL: " + final_url + "\nDownloading: " + filename + "\n")
+                                    os.system(download)
+                                else:
+                                    continue
 
-def main():
-    url = "http://10.10.10.248/documents/2020-"
-    get_pedfs(url)
+            elif month_first_digit == 1:
+                for month_second_digit in range(0,3):
+                    for day_first_digit in range(0,4):
+                        if day_first_digit == 3:
+                            for day_second_digit in range(0,2):
+                                final_url = self.url + str(month_first_digit) + str(month_second_digit) + "-" + \
+                                str(day_first_digit) + str(day_second_digit) + "-upload.pdf"
+
+                                req_pdf = requests.get(final_url)
+                                if req_pdf.status_code != 404:
+                                    filename = final_url.rsplit('/',1)[1]
+                                    download = "curl " + final_url + " -o " + filename + " >/dev/null 2>&1"
+                                    print("Found file in URL: " + final_url + "\nDownloading: " + filename + "\n")
+                                    os.system(download)
+                                else:
+                                    continue
+                        else:
+                            for day_second_digit in range(0,10):
+                                final_url = self.url + str(month_first_digit) + str(month_second_digit) + "-" + \
+                                    str(day_first_digit) + str(day_second_digit) + "-upload.pdf"
+                                    
+                                req_pdf = requests.get(final_url)
+                                if req_pdf.status_code != 404:
+                                    filename = final_url.rsplit('/',1)[1]
+                                    download = "curl " + final_url + " -o " + filename + " >/dev/null 2>&1"
+                                    print("Found file in URL: " + final_url + "\nDownloading: " + filename + "\n")
+                                    os.system(download)
+                                else:
+                                    continue
 
 if __name__ == "__main__":
-    main()
+    url = "http://10.10.10.248/documents/2020-"
+    Intelligence(url)
